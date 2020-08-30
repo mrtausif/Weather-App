5e8e145d70c2966c6fa4e0445e9001cbfde9320c
mrtausif <sayhellotoopi@gmai.com>
2020-08-31 02:39:06 +0600

    app for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     const adress = search.value;
     if (!adress) return (msgOne.textContent = 'Please input adress');
     msgOne.textContent = 'Loading...';
-    fetch(`http://localhost:3000/weather?adress=${adress}`).then((res) => {
+    fetch(`/weather?adress=${adress}`).then((res) => {
         res.json().then((data) => {
             if (data.error) return (msgOne.textContent = data.error);
             msgOne.textContent = data.location + ' ' + data.forecast;
